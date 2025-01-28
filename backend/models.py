1f8f27806463c91463dc20c6c7e8343be25706c2
Balaji Udayasuiryan <balajiudayasuriyan1102@gmail.com>
2025-01-28 19:16:02 +0530

    Add category model

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# category model
+
+class Category (models.Model):
+
+    id = models.BigAutoField(primary_key=True)
+
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'category'
+
