1aaab069054168a5fd14a382daa9a5ce718f9862
Balaji Udayasuiryan <balajiudayasuriyan1102@gmail.com>
2025-01-28 19:35:05 +0530

    add brand model and brand status in models.py

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
 
 # category model
 
@@ -13,4 +16,32 @@ class Category (models.Model):
 
     class Meta:
         db_table = 'category'
+
+# brand model
+
+class BrandStatus(models.TextChoices):
+
+    PUBLISHED = 'PUBLISHED', _('PUBLISHED')
+    DRAFT = 'DRAFT', _('DRAFT')
+    PENDING = 'PENDING', _('PENDING')
+
+class Brand(models.Model):
+
+    id = models.BigAutoField(primary_key=True)
+
+    name = models.CharField(max_length=255)
+
+    image_path = models.ImageField(upload_to='brand', null=True, blank=True, default='No_image_available.jpg')
+
+    status = models.CharField(
+        max_length=255,
+        choices=BrandStatus.choices,
+        default=BrandStatus.PUBLISHED
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'brand'
 
