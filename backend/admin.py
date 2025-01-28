f726d667061d77145bdca9f69a0ca36102c0f9a7
Balaji Udayasuiryan <balajiudayasuriyan1102@gmail.com>
2025-01-28 19:42:56 +0530

    Update admin.py
    add category filters in admin.py

--- backend/admin.py
+++ backend/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Brand
 
 # Register your models here.
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
 
+    list_display = ('id','name')
+
+    search_fields = ('id','name',)
+
+    ordering = ['id']
+
+admin.site.register(Category,CategoryAdmin)
+
+admin.site.register(Brand)
