6991c937ec06744e1a1acbfe9fadf5ea79fcc3d9
Balaji Udayasuiryan <balajiudayasuriyan1102@gmail.com>
2025-01-29 19:09:40 +0530

    Add product model

--- backend/models.py
+++ backend/models.py
@@ -45,3 +45,32 @@ class Brand(models.Model):
     class Meta:
         db_table = 'brand'
 
+# product model
+
+class Product(models.Model):
+
+    id = models.BigAutoField(primary_key= True)
+
+    name = models.CharField(max_length=255)
+
+    image_path = models.ImageField(upload_to='product', null=True, blank=True, default='No_image_available.jpg' )
+
+    description =models.TextField()
+
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+
+    brand = models.ForeignKey(Brand, null=True, blank=True, on_delete=models.SET_NULL)
+
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+
+    qty = models.IntegerField(null=True, blank=True)
+
+    alert_stock = models.IntegerField(null=True, blank=True)
+
+    image_path = models.ImageField(upload_to='product', null=True, blank=True, default='no-image-available.jpg')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'product'
