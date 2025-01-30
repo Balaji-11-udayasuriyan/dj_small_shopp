4308dec7df81ed649946b55732c355501389411c
Balaji Udayasuiryan <balajiudayasuriyan1102@gmail.com>
2025-01-30 18:15:33 +0530

    add cart and order in admin.py

--- backend/admin.py
+++ backend/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Category, Brand
+from .models import Category, Brand, Product, Cart, Order
+
 
 # Register your models here.
 
@@ -26,3 +27,39 @@ class BrandAdmin(admin.ModelAdmin):
     ordering = ['id']
 
 admin.site.register(Brand,BrandAdmin)
+
+# product admin
+
+class ProductAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'name', 'image_path','description', 'category', 'brand','price', 'qty','alert_stock',)
+
+    search_fields = ('id', 'name',)
+
+    ordering = ['id']
+
+admin.site.register(Product,ProductAdmin)
+
+# cart admin
+
+class CartAdmin(admin.ModelAdmin):
+
+    list_display = ('id','product','qty',)
+
+    search_fields = ('id','product',)
+
+    ordering = ['id']
+
+admin.site.register(Cart,CartAdmin)
+
+# order admin
+
+class OrderAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'order_number', 'order_date', 'total_amount', 'order_status', 'payment_method',)
+
+    search_fields = ('id', 'order_number','order_status','payment_method')
+
+    ordering = ['id']
+
+admin.site.register(Order, OrderAdmin)
