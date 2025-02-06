30a8081635793534131f458a8acda564de5c67bb
Balaji Udayasuiryan <balajiudayasuriyan1102@gmail.com>
2025-02-06 11:25:32 +0530

    add api_v1,rest_framework

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'backend'
+    'backend',
+    'rest_framework',
+    'api_v1'
+
 ]
 
 MIDDLEWARE = [
