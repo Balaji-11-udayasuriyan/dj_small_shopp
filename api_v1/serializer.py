0bc980f6786d5a2fe0e473ae4859b31ecca2ed16
Balaji Udayasuiryan <balajiudayasuriyan1102@gmail.com>
2025-02-06 11:30:30 +0530

    Add category and brand in serializers.py

---
+++ api_v1/serializer.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from backend.models import Category, Brand, Product
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+class BrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = '__all__'
+
