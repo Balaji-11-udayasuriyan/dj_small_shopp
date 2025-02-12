5c974f4b3c89e3d8755108c95eebf03cf2ad3b3d
Balaji Udayasuiryan <balajiudayasuriyan1102@gmail.com>
2025-02-12 12:00:16 +0530

    add brand, product and cart router

--- api_v1/urls.py
+++ api_v1/urls.py
@@ -2,11 +2,18 @@ from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 
-from .views import CategoryViewSet
+from .serializer import CartSericalizer
+from .views import CategoryViewSet, BrandViewSet, ProductViewSet, CartViewSet
 
 router = DefaultRouter()
 
 router.register(r'categories', CategoryViewSet, basename='category')
+
+router.register(r'brands', BrandViewSet, basename='brand')
+
+router.register(r'products', ProductViewSet, basename='product')
+
+router.register(r'carts', CartViewSet, basename='cart')
 
 urlpatterns = [
     path('', include(router.urls)),
