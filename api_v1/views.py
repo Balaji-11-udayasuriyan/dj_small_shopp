135202851f20bbbd24d001421cef94310707bf7f
Balaji Udayasuiryan <balajiudayasuriyan1102@gmail.com>
2025-02-12 12:01:50 +0530

    add cartviewset in views.py

--- api_v1/views.py
+++ api_v1/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 
-from api_v1.serializer import CategorySerializer, BrandSerializer, ProductSerializer
-from backend.models import Category, Brand, Product
+from api_v1.serializer import CategorySerializer, BrandSerializer, ProductSerializer, CartSericalizer
+from backend.models import Category, Brand, Product, Cart
 
 
 # Create your views here.
@@ -103,3 +103,35 @@ class ProductViewSet(viewsets.ModelViewSet):
         instance = self.get_object()
         self.perform_destroy(instance)
         return Response({'message': 'Product deleted successfully'})
+
+class CartViewSet(viewsets.ModelViewSet):
+    queryset = Cart.objects.all()
+    serializer_class = CartSericalizer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        data = {
+            'data': serializer.data
+        }
+        return Response(data)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            return Response({'message': 'Cart created successfully'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        if serializer.is_valid():
+            self.perform_update(serializer)
+            return Response({'message': 'Cart updated successfully'})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response({'message': 'Cart deleted successfully'})
