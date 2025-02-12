7742c3eb71c88b2da438ce24195d69c416e0a80b
Balaji Udayasuiryan <balajiudayasuriyan1102@gmail.com>
2025-02-12 11:58:37 +0530

    add cart serializer

--- api_v1/serializer.py
+++ api_v1/serializer.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from backend.models import Category, Brand, Product
+from backend.models import Category, Brand, Product, Cart
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -16,6 +16,11 @@ class BrandSerializer(serializers.ModelSerializer):
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Brand
+        model = Product
+        fields = '__all__'
+
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cart
         fields = '__all__'
 
